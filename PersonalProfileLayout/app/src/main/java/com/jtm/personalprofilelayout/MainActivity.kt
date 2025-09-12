daebc45726e243256e86912ad27b02f2053c03bd
JeffersonMonteiro <jefferson.monteiro@i2s.pt>
2025-09-12 10:35:57 +0930

    Initial Commit

--- PersonalProfileLayout/app/src/main/java/com/jtm/personalprofilelayout/MainActivity.kt
+++ PersonalProfileLayout/app/src/main/java/com/jtm/personalprofilelayout/MainActivity.kt
@@ -4,13 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jtm.personalprofilelayout.ui.theme.PersonalProfileLayoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +35,82 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PersonalProfileLayoutTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MyApp()
+
         }
     }
 }
 
 @Composable
+fun MyApp() {
+    PersonalProfileLayoutTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background) {
+            Greeting("Android")
+        }
+    }
+}
+
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+    ){
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+
+        Text(
+            text = "Hello Kotlin",
+            modifier = modifier
+        )
+
+        Text(
+            text = "Hello Compose",
+            modifier = modifier
+        )
+        Text(
+            text = "Hello World",
+            modifier = modifier
+        )
+        Row (
+            modifier = Modifier.fillMaxWidth().background(Color.Gray),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Icon(
+                Icons.Default.Call,
+                contentDescription = null,
+                modifier = Modifier.size(40.dp)
+            )
+            Text(text = "CALL ME")
+        }
+        Box (
+            modifier = Modifier.background(Color.Green).size(200.dp),
+            contentAlignment = Alignment.BottomEnd
+        ) {
+            Icon(
+                Icons.Default.Delete,
+                contentDescription = null,
+                modifier = Modifier.size(40.dp)
+            )
+
+            Icon(
+                Icons.Default.Build,
+                contentDescription = null,
+                modifier = Modifier.size(40.dp).align(Alignment.Center)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    PersonalProfileLayoutTheme {
-        Greeting("Android")
-    }
+    MyApp()
 }
